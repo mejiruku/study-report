2db4faa0364002a4921fbe22e465a4a5a22efb97
Anto <83343186+mejiruku@users.noreply.github.com>
2026-02-19 00:24:10 +0900

    Add files via upload

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,17 +1,19 @@
-// Firebase Configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDXrKDl-NmTWQ6TgW6G6O1LeHQXH1RIv1Y",
-  authDomain: "studyreport-dc715.firebaseapp.com",
-  projectId: "studyreport-dc715",
-  storageBucket: "studyreport-dc715.firebasestorage.app",
-  messagingSenderId: "1031578888568",
-  appId: "1:1031578888568:web:7e6ec22fc674f704ee808d",
-  measurementId: "G-CQ340DVK9R",
+  apiKey: "AIzaSyCLJG-ExZC7dVOcSL0Tfzdv965ewCj_uGs",
+  authDomain: "flipcard-318f3.firebaseapp.com",
+  projectId: "flipcard-318f3",
+  storageBucket: "flipcard-318f3.firebasestorage.app",
+  messagingSenderId: "517432601240",
+  appId: "1:517432601240:web:9df881ed3f3c3b3bbb3eed",
+  measurementId: "G-BGE5TKN2QP"
 };
 
-// Initialize Firebase
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-window.db = firebase.firestore();
-window.auth = firebase.auth();
-window.provider = new firebase.auth.GoogleAuthProvider();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
